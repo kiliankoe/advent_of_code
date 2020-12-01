000000004a74b0a645469a01af9c1f78885ec72b
Kilian Koeltzsch <me@kilian.io>
2020-12-01 09:08:51 +0100

    day 1

---
+++ day1/Sources/Day1/main.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+let inputFile = Bundle.module.url(forResource: "input", withExtension: "txt")!
+let input = try! String(contentsOf: inputFile)
+    .split(separator: "\n")
+    .map(String.init)
+    .compactMap(Int.init)
+
+for entry in input {
+    for match in input {
+        for match2 in input {
+            if entry + match + match2 == 2020 {
+                print(entry, match, match2, entry * match * match2)
+                exit(0)
+            }
+        }
+    }
+}
