00000090f6b42d3f3a6dec7d99550b1f167183e3
Kilian Koeltzsch <me@kilian.io>
2020-12-05 17:31:17 +0100

    day 5 use range w/ lower-/upperHalf

--- Sources/Day5/main.swift
+++ Sources/Day5/main.swift
@@ -1,28 +1,31 @@
 import Foundation
 import Util
 
+extension ClosedRange where Bound == Int {
+    var lowerHalf: ClosedRange {
+        lowerBound...(lowerBound + count / 2)
+    }
+
+    var upperHalf: ClosedRange {
+        (lowerBound + count / 2)...upperBound
+    }
+}
+
 func decode(boardingPass: String) -> (row: Int, column: Int) {
-    var rows = Array(0...127)
-    for (idx, char) in boardingPass.prefix(7).enumerated() {
-        let valueCount = Int(pow(2.0, Double(6 - idx)))
-        if char == "F" {
-            rows = Array(rows.prefix(valueCount))
-        } else {
-            rows = Array(rows.suffix(valueCount))
+    var row = 0...127
+    var column = 0...7
+
+    for character in boardingPass {
+        switch character {
+        case "F": row = row.lowerHalf
+        case "B": row = row.upperHalf
+        case "L": column = column.lowerHalf
+        case "R": column = column.upperHalf
+        default: fatalError()
         }
     }
 
-    var columns = Array(0...7)
-    for (idx, char) in boardingPass.suffix(3).enumerated() {
-        let valueCount = Int(pow(2.0, Double(2 - idx)))
-        if char == "L" {
-            columns = Array(columns.prefix(valueCount))
-        } else {
-            columns = Array(columns.suffix(valueCount))
-        }
-    }
-
-    return (rows[0], columns[0])
+    return (row.lowerBound, column.lowerBound)
 }
 
 func checksum(_ boardingPass: (row: Int, column: Int)) -> Int {
